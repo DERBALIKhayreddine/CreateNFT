8c34d038ef04735fab08525106b3bcb7dbd0390f
DERBALIKhayreddine <khariddineiit@gmail.com>
2024-12-12 00:02:59 +0100

    Create a Non-Fungible Token code)

--- index.js
+++ index.js
@@ -49,8 +49,35 @@ async function environmentSetup() {
 
     console.log("The new account balance is : " + accountBalance.hbars.toTinybars() + " Tinybars");
 
+    //Create the NFT
+    const nftCreate = await new TokenCreateTransaction()
+        .setTokenName("diploma")
+        .setTokenSymbol("GRAD")
+        .setTokenType(TokenType.NonFungibleUnique)
+        .setDecimals(0)
+        .setInitialSupply(0)
+        .setTreasuryAccountId(treasuryId)
+        .setSupplyType(TokenSupplyType.Finite)
+        .setMaxSupply(250)
+        .setSupplyKey(supplyKey)
+        .freezeWith(client);
 
-    
+    //Sign the transaction with the treasury key
+    const nftCreateTxSign = await nftCreate.sign(treasuryKey);
+
+    //Submit the transaction to a Hedera network
+    const nftCreateSubmit = await nftCreateTxSign.execute(client);
+
+    //Get the transaction receipt
+    const nftCreateRx = await nftCreateSubmit.getReceipt(client);
+
+    //Get the token ID
+    const tokenId = nftCreateRx.tokenId;
+
+    //Log the token ID
+    console.log("Created NFT with Token ID: " + tokenId);
+
+
 
 
 }
